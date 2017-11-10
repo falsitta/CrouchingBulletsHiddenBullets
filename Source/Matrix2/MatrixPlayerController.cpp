3fb66634bea624f965500508d076f5ef484a90d2
falsitta <nickfalsitta@gmail.com>
2017-11-09 19:43:16 -0500

    first commit

---
+++ Source/Matrix2/MatrixPlayerController.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Matrix2.h"
+#include "MatrixPlayerController.h"
+
+
+
+
---
+++ Source/Matrix2/MatrixPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "MatrixPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATRIX2_API AMatrixPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
